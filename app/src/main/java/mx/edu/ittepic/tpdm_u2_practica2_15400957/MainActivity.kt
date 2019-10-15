f8544263fe7821a2e8b6c04d0a6d4f9f1f2474ea
aguilar2503 <aguilar_.roberto@hotmail.com>
2019-10-14 21:42:51 -0600

    creacion de tablas

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u2_practica2_15400957/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ittepic.tpdm_u2_practica2_15400957
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
